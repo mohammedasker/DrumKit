2aad7fcf6f70c920f500aa373362f37a8d3fed92
Mohammed Asker <zetoanime@gmail.com>
2019-08-08 09:43:06 +0300

    add sound files, background img & play audio when clicked the keys

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-window.addEventListener('keydown', function(e) {
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	if(!audio) return; // stop the function from working all together
-			console.log(audio);
-		});
+  
+  function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return; // stop the function from working all together
+  audio.currentTime = 0; // Rewind to the start
+  audio.play();
+  key.classList.add('playing');
+  }
+  function removeTransition(e) {
+  	if (e.propertyName != 'transform') return; // skip if it's not a transform
+  	this.classList.remove('playing');
+  }
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
